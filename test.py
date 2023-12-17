b95a2b94beb756192d6c0c1e72417af16b3ecc5b
Vinay Kumar Manchala <manchalavinaykumar478@gmail.com>
2023-12-17 16:41:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
-from common_utilities.aes_encryption import AESCipher
 
-decrypted_password = AESCipher(key="I Seek Vengeance").decrypt("ImVc0dsbgq4Serf6K0219G9JFAYxMVt9boH+JHV/v5c=")
-print(decrypted_password)
-print(f"decrypted password is {decrypted_password}")
